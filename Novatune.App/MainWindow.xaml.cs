931d746344010e480bfa500d7a651781a2685936
tribeti <117581928+tribeti@users.noreply.github.com>
2026-06-03 18:13:09 +0700

    add pages + nav

--- Novatune.App/MainWindow.xaml.cs
+++ Novatune.App/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 using DevWinUI;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
+using System;
 
 namespace Novatune.App;
 
@@ -9,5 +12,32 @@ public sealed partial class MainWindow : Window
     {
         InitializeComponent();
         DragMoveAndResizeHelper.SetDragMove(this, Root);
+    }
+
+    private void NavigationBar_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.IsSettingsSelected == true)
+        {
+            NavView_Navigate(typeof(Views.SettingPage), args.RecommendedNavigationTransitionInfo);
+        }
+        else if (args.SelectedItemContainer is not null)
+        {
+            Type? navPageType = Type.GetType(args.SelectedItemContainer.Tag.ToString()!);
+            NavView_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
+        }
+    }
+
+    private void ContentFrame_NavigationFailed(object sender, Microsoft.UI.Xaml.Navigation.NavigationFailedEventArgs e)
+    {
+        throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+    }
+
+    private void NavView_Navigate(Type? navPageType, NavigationTransitionInfo transitionInfo)
+    {
+        Type preNavPageType = ContentFrame.CurrentSourcePageType;
+        if (navPageType is not null && !Type.Equals(preNavPageType, navPageType))
+        {
+            ContentFrame.Navigate(navPageType, null, transitionInfo);
+        }
     }
 }
