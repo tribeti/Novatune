db02a3af991267a0fe153d28f175d23fdb4b2f38
tribeti <117581928+tribeti@users.noreply.github.com>
2026-06-09 18:07:52 +0700

    add command

--- Novatune.App/ViewModels/MediaViewModel.cs
+++ Novatune.App/ViewModels/MediaViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Dispatching;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.Media.Core;
 using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 namespace Novatune.App.ViewModels;
 
 public partial class MediaViewModel : BaseViewModel
 {
-    public MediaPlayer Player = new();
+    public MediaSource? mediaSource;
+    MediaPlaybackList mediaPlaybackList = new();
+    public MediaPlayer mediaPlayer = new();
+    public ObservableCollection<StorageFile> MediaFiles { get; } = new();
 
     [ObservableProperty]
     public partial bool IsPlaying { get; set; } = false;
@@ -15,21 +25,80 @@ public partial class MediaViewModel : BaseViewModel
 
     public MediaViewModel()
     {
-        Player.PlaybackSession.PlaybackStateChanged += (s, _) =>
+        mediaPlayer.PlaybackSession.PlaybackStateChanged += (s, _) =>
         {
             _dispatcherQueue.TryEnqueue(() =>
             {
                 IsPlaying = s.PlaybackState == MediaPlaybackState.Playing;
             });
         };
+        mediaPlaybackList.MaxPlayedItemsToKeepOpen = 3;
+        mediaPlayer.Source = mediaPlaybackList;
     }
 
     [RelayCommand]
     public void PlayPause()
     {
         if (IsPlaying)
-            Player.Pause();
+            mediaPlayer.Pause();
         else
-            Player.Play();
+            mediaPlayer.Play();
+    }
+
+    [RelayCommand]
+    public async Task AddMedia()
+    {
+        var picker = new FileOpenPicker
+        {
+            SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+            ViewMode = PickerViewMode.List,
+            FileTypeFilter = { ".wmv", ".mp4", ".mkv", ".mp3", ".flac" },
+        };
+
+        var mainWindow = (App.Current as App)!.MainWindow;
+        var hwnd = WindowNative.GetWindowHandle(mainWindow);
+        InitializeWithWindow.Initialize(picker, hwnd);
+
+        var files = await picker.PickMultipleFilesAsync();
+
+        if (files is not null)
+        {
+            foreach (var file in files)
+            {
+                var storageFile = await StorageFile.GetFileFromPathAsync(file.Path);
+                var mediaPlaybackItem = new MediaPlaybackItem(MediaSource.CreateFromStorageFile(storageFile));
+                mediaPlaybackList.Items.Add(mediaPlaybackItem);
+                MediaFiles.Add(file);
+            }
+        }
+    }
+
+    [RelayCommand]
+    public void Previous() => mediaPlaybackList.MovePrevious();
+
+    [RelayCommand]
+    public void Next() => mediaPlaybackList.MoveNext();
+
+
+    [RelayCommand]
+    public void Shuffle()
+    {
+        mediaPlaybackList.ShuffleEnabled = !mediaPlaybackList.ShuffleEnabled;
+
+        //DispatcherQueue.TryEnqueue(() =>
+        //{
+        //    Shuffle_Btn.FontWeight = mediaPlaybackList.ShuffleEnabled ? Microsoft.UI.Text.FontWeights.Bold : Microsoft.UI.Text.FontWeights.Light;
+        //});
+    }
+
+    [RelayCommand]
+    public void Repeat()
+    {
+        mediaPlaybackList.AutoRepeatEnabled = !mediaPlaybackList.AutoRepeatEnabled;
+
+        //DispatcherQueue.TryEnqueue(() =>
+        //{
+        //    autoRepeatButton.FontWeight = mediaPlaybackList.AutoRepeatEnabled ? Microsoft.UI.Text.FontWeights.Bold : Microsoft.UI.Text.FontWeights.Light;
+        //});
     }
 }
--- Novatune.App/Views/HomePage.xaml.cs
+++ Novatune.App/Views/HomePage.xaml.cs
@@ -1,9 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Navigation;
 using Novatune.App.ViewModels;
-using System;
-using Windows.Media.Core;
 
 namespace Novatune.App.Views;
 
@@ -15,12 +12,6 @@ public sealed partial class HomePage : Page
         InitializeComponent();
         ViewModel = App.Current.Services.GetService<MediaViewModel>()!;
         DataContext = ViewModel;
-    }
-
-    protected override void OnNavigatedTo(NavigationEventArgs e)
-    {
-        base.OnNavigatedTo(e);
-        mediaPlayerElement.SetMediaPlayer(ViewModel.Player);
-        ViewModel.Player.Source = MediaSource.CreateFromUri(new Uri("D:\\VS\\tribeti\\Novatune\\Novatune.App\\Assets\\audio.mp3"));
+        mediaPlayerElement.SetMediaPlayer(ViewModel.mediaPlayer);
     }
 }
